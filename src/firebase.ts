6407351181014b96fc4fad3e83ab16a170296f76
eletronicosjoinville-design <eletronicosjoinville@gmail.com>
2026-04-07 20:17:27 -0300

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,15 +3,20 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC73c7j6TTXFXQp0paBy-PyEAXbkhw80ow", // Esta chave é essencial
+  apiKey: "AIzaSyC73c7j6TTXfXQp0paBy-PyEAXbkhw80ow",
   authDomain: "project--infosmart.firebaseapp.com",
+  databaseURL: "https://project--infosmart-default-rtdb.firebaseio.com",
   projectId: "project--infosmart",
   storageBucket: "project--infosmart.firebasestorage.app",
   messagingSenderId: "649887732683",
   appId: "1:649887732683:web:fb08a26cc97c12e8981c4e",
-  measurementId: "G-P09CE148KG"
+  measurementId: "G-P09CE140KG"
 };
 
+// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
+
+// Exporta os serviços para usar no resto do site
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export default app;
